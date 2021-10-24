c47fbddcef7269e5512361a6237c3291c7a42ef5
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2021-10-24 15:57:22 +0800

    Create YOUTUBE.sh

---
+++ YOUTUBE.sh
@@ -0,0 +1,15 @@
+wget https://cdn.discordapp.com/attachments/853535040250970113/878590395611775016/yt.zip
+unzip yt.zip
+unzip root.zip
+tar -xvf root.tar.xz
+wget https://raw.githubusercontent.com/GranGuorgeMC/DOWNLOADS-FILES/main/Playitgg.sh
+chmod +x Playitgg.sh
+./Playitgg.sh
+mkdir mcpe
+cd mcpe
+mkdir bedrock
+mkdir pocketmine
+echo 'done- COPY this'
+echo "./dist/proot -S . /bin/bash'
+echo 'then type'
+echo 'bash run.sh'
