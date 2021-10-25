b66d9bab7faa52e5f2b1bad152dd0a7f09e6fa22
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2021-10-25 13:59:42 +0800

    Create the.sh

---
+++ the.sh
@@ -0,0 +1,5 @@
+cd bedrock-mcpe
+mkdir worlds
+cd worlds
+wget https://raw.githubusercontent.com/GranGuorgeMC/MINECRAFT-SERVER/main/runner.sh
+wget https://raw.githubusercontent.com/GranGuorgeMC/MINECRAFT-SERVER/main/runner2.sh
