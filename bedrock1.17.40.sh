df81be25e392487e00f3949d18c322a4cb59c2b2
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2021-10-25 13:28:02 +0800

    Create bedrock1.17.40.sh

---
+++ bedrock1.17.40.sh
@@ -0,0 +1,6 @@
+cd mcpe
+cd bedrock
+wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40.06.zip
+unzip bedrock-server-1.17.40.06.zip
+rm bedrock-server-1.17.40.06.zip
+echo "now you can run ./run.sh"
